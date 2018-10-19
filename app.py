b7c278e85f1100b63322c0c13014634504ea95f2
GokulVSD <gokul.vasda@gmail.com>
2018-10-20 01:13:09 +0530

    Too many changes to list

--- app.py
+++ app.py
@@ -14,56 +14,16 @@ def verifyAndRenderRespective():
 	password = request.form['password']
 
 	try:
-		if username == 'booker' and password == 'placeholder':
-			return render_template('booker.html')
-		elif username == 'manager' and password == 'placeholder':
+		if username == 'cashier' and password == 'a':
+			return render_template('cashier.html')
+		elif username == 'manager' and password == 'a':
 			return render_template('manager.html')
 		else:
 			return render_template('loginfail.html')
 	except:
 		return render_template('loginfail.html')
 
-# @app.route("/insert", methods = ['POST'])
-# def insert():
-#     uname=request.form.get('uname')
-#     pswd=request.form.get('pswd')
-#     sql="INSERT INTO movies values(%s,%s,%s)"
-#     val=(request.form.get('name'),request.form.get('lang'),request.form.get('len'))
-#     return query(uname,pswd,sql,val)
 
-# @app.route("/update", methods = ['POST'])
-# def update():
-#     uname=request.form.get('uname')
-#     pswd=request.form.get('pswd')
-#     sql="UPDATE movies SET "+request.form.get('cont')+" WHERE "+request.form.get('cond')
-#     val=None
-#     return query(uname,pswd,sql,val)
-
-# @app.route("/delete", methods = ['POST'])
-# def delete():
-#     uname=request.form.get('uname')
-#     pswd=request.form.get('pswd')
-#     sql="DELETE FROM movies WHERE "+request.form.get('cond')
-#     val=None
-#     return query(uname,pswd,sql,val)
-
-# def query(uname,pswd,sql,val):
-#     try:
-#         conobj = mysql.connector.connect(host='localhost',
-#                                        database='test',
-#                                        user=uname,
-#                                        password=pswd)
-#         if conobj.is_connected():
-#             cursor = conobj.cursor()
-#             cursor.execute(sql,val)
-#             conobj.commit()
-#             conobj.close()
-#             return jsonify({"res" : "Success"})
-#     except Error as e:
-#         return jsonify({"res" : e.args[1]})
-#     finally:
-#         conobj.close()
-#     return jsonify({"res" : "failed to connect to MySQL"})
 
 if __name__ == "__main__":
     app.run()
--- static/app.js
+++ static/app.js
@@ -1,61 +1,21 @@
-// function ins(){
-// 	var name = $("#inname")[0].value;
-// 	var lang = $("#inlang")[0].value;
-// 	var len = $("#inlen")[0].value;
-// 	send("ins",[name,lang,len]);
-// }
-// function del(){
-// 	var cond = $("#delcond")[0].value;
-// 	send("del",cond);
-// }
-// function upd(){
-// 	var cont = $("#updcont")[0].value;
-// 	var cond = $("#updcond")[0].value;
-// 	send("upd",[cont,cond]);
-// }
-// function send(type,params){
-// 	var qtype,d;
-// 	if(type==="upd"){
-// 		qtype = "/update";
-// 		d = {'uname':$("#uname")[0].value,
-// 			'pswd':$("#pswd")[0].value,
-// 			'cont':params[0],
-// 			'cond':params[1]};
-// 	}
-// 	if(type==="del"){
-// 		qtype = "/delete";
-// 		d = {'uname':$("#uname")[0].value,
-// 			'pswd':$("#pswd")[0].value,
-// 			'cond':params};
-// 	}
-// 	if(type==="ins"){
-// 		qtype = "/insert";
-// 		d = {'uname':$("#uname")[0].value,
-// 			'pswd':$("#pswd")[0].value,
-// 			'name':params[0],
-// 			'lang':params[1],
-// 			'len':params[2]};
-// 	}
-// 	console.log(qtype);
-// 	console.log(d);
-// 	$.ajax({
-// 		type: "POST",
-// 		url: qtype,
-// 		data: d,
-// 		success: function(data){
-// 			var res = data['res'];
-// 			$("#status")[0].innerHTML="Status: "+res;
-// 		},
-// 		error: function(data){
-// 			$("#status")[0].innerHTML="Status: Login failure";
-// 		}
-// 	});
-// }
+var username = null;
+var password = null;
+var date = null;
+var movieID = null;
+var type = null;
+var seatNo = null;
+var seatClass = null;
+var movieTime = null;
+var showID = null;
 
 function login(){
+	if(username === null){
+		username = $("[name='username']")[0].value;
+		password = $("[name='password']")[0].value;
+	}
 	var form = {
-		'username' : $("[name='username']")[0].value,
-		'password' : $("[name='password']")[0].value
+		'username' : username,
+		'password' : password
 	};
 	$.ajax({
 		type: 'POST',
@@ -63,7 +23,110 @@ function login(){
 		data: form,
 		success: function(response){
 			$('#dynamic').html(response);
-			$('.login-header').toggleClass('after-login');
+			$('.login-header').addClass('after-login');
+			$('.module').addClass('module-after-login');
+			$('#datepicker-cashier').pickadate({
+				min : new Date(),
+				formatSubmit: 'yyyy/mm/dd',
+ 				hiddenName: true,
+ 				onSet: function( event ) {
+ 					if ( event.select ) {
+ 						$('#datepicker-cashier').prop('disabled', true);
+ 						getMoviesShowingOnDate(this.get('select', 'yyyy/mm/dd' ));
+ 					}
+ 				}
+			});
+		}
+	});
+}
+
+function getMoviesShowingOnDate(mdate){
+	date = mdate;
+	$.ajax({
+		type: 'POST',
+		url: '/getMoviesShowingOnDate',
+		data: {'date' : mdate},
+		success: function(response){
+			$('#movies-on-date').html(response);
+		}
+	});
+}
+
+function selectMovie(movID, mtype){
+	movieID = movID;
+	type = mtype;
+	$.ajax({
+		type: 'POST',
+		url: '/getTimings',
+		data: {
+			'date' : mdate,
+			'movieID': movieID,
+			'type' : type
+		},
+		success: function(response){
+			$('#movies-on-date button').prop('disabled', true);
+			$('#timings-for-movie').html(response);
+		}
+	});
+}
+
+function selectTiming(mtime){
+	movieTime = mtime;
+	$.ajax({
+		type: 'POST',
+		url: '/getShowID',
+		data: {
+			'date' : mdate,
+			'movieID': movieID,
+			'type' : type,
+			'time' : movieTime
+		},
+		success: function(response){
+			$('#timings-for-movie button').prop('disabled', true);
+			showID = response['showID'];
+			getSeats();
+		}
+	});
+}
+
+function getSeats(){
+	$.ajax({
+		type: 'POST',
+		url: '/getAvailableSeats',
+		data: {'showID' : showID},
+		success: function(response){
+			$('#available-seats').html(response);
+		}
+	});
+}
+
+function selectSeat(no, sclass){
+	seatNo = no;
+	seatClass = sclass;
+	$.ajax({
+		type: 'POST',
+		url: '/getPrice',
+		data: {
+			'showID' : showID,
+			'seatClass' : seatClass
+			},
+		success: function(response){
+			$('available-seats button').prop('disabled', true);
+			$('#price-and-confirm').html(response);
+		}
+	});
+}
+
+function confirmBooking(){
+	$.ajax({
+		type: 'POST',
+		url: '/insertBooking',
+		data: {
+			'showID' : showID,
+			'seatNo' : seatNo
+			},
+		success: function(response){
+			$('#price-and-confirm').html(response);
 		}
 	});
 }
